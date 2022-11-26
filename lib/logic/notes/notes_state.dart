da9ab066e7904616f7b926c66f7c4fc92d937330
akhmadjonakbarov <akhmadjonakbarov@gmail.com>
2022-11-26 13:24:13 +0500

    all states was updated

--- lib/logic/notes/notes_state.dart
+++ lib/logic/notes/notes_state.dart
@@ -4,27 +4,37 @@ part of 'notes_cubit.dart';
 
 @immutable
 abstract class NotesState {
+  // this is abstract class for State
   List<Note>? notes;
   NotesState({this.notes});
 }
 
-class NotesWelcome extends NotesState {}
+class NotesWelcome extends NotesState {
+  // this is Notes Welcome class. If we don't have anything, we will return these.
+}
 
-class NotesInitial extends NotesState {}
+class NotesInitial extends NotesState {
+  // this class for initialing notes.
+}
 
-class NotesLoading extends NotesState {}
+class NotesLoading extends NotesState {
+  // this class for loading notes. If we add new notes, we will
+}
 
 class NotesLoaded extends NotesState {
+  // this class for reloading data.
   List<Note>? notes;
   NotesLoaded({required this.notes}) : super(notes: notes);
 }
 
 class NoteAdded extends NotesState {
+  // this class for additing new notes.
   final Note note;
   NoteAdded(this.note);
 }
 
 class NotesDelete extends NotesState {
+  // this class for deleting.
   List<Note>? notes;
   NotesDelete({this.notes}) : super(notes: notes);
 }
